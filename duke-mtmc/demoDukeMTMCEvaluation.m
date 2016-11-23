82ed11999e684d7e33ed98198ad1f4ec50c93a72
amilan <devnull@localhost>
2016-11-23 21:21:43 +1030

    initial dump

---
+++ duke-mtmc/demoDukeMTMCEvaluation.m
@@ -0,0 +1,43 @@
+addpath('devkit');
+
+trackerOutput = dlmread('res/baseline.txt');
+
+world = false; % Image plane
+iou_threshold = 0.5;
+testSets = {'easy', 'hard'};  % 'trainval'
+
+% Evaluate
+for i = 1:length(testSets)
+    testSet = testSets{i};
+    results{i} = evaluateDukeMTMC(trackerOutput, iou_threshold, world, testSet);
+
+end
+
+%% Display
+% load('results.mat'); % Precomputed
+
+% Print
+for i = 1:length(testSets)
+    
+    result = results{i};
+    fprintf('\n-------Results-------\n');
+    fprintf('Test set: %s\n', testSets{i});
+    % Single cameras all
+    fprintf('%s\n',result{9}.description);
+    printMetrics(result{9}.allMets.mets2d.m); 
+    
+    % Multi-cam
+    k = 10;    
+    fprintf(result{k}.description);
+    fprintf('\tIDF1: %.2f', result{k}.IDF1);
+    fprintf('\tIDP: %.2f', result{k}.IDP);
+    fprintf('\tIDR: %.2f\n', result{k}.IDR);
+    
+    % All individual cameras
+    fprintf('\n'); 
+    for k = 1:8
+       fprintf('%s\n',result{k}.description);
+       printMetrics(result{k}.allMets.mets2d.m); 
+    end
+
+end
---
+++ duke-mtmc/evaluateDukeMTMC.m
@@ -0,0 +1,98 @@
+function result = evaluateDukeMTMC(resMat, iou_threshold, world, testSet)
+
+ROI = getROIs();
+
+if strcmp(testSet,'easy')
+    load('gt/testData.mat');
+    gtMat = testData;
+    testInterval = [263504:356648];
+elseif strcmp(testSet,'hard')
+    load('gt/testHardData.mat');
+    gtMat = testHardData;
+    testInterval = [227541:263503];
+elseif strcmp(testSet,'trainval')
+    load('gt/trainval.mat');
+    gtMat = trainDataClipped;
+    testInterval = [47720:227540]; % takes too long, use smaller interval
+else
+    fprintf('Unknown test set %s\n',testSet);
+    return;
+end
+
+
+
+% Change frames to global time
+startTimes = [5543, 3607, 27244, 31182, 1, 22402, 18968, 46766];
+frames = resMat(:,3);
+for cam = 1:8
+    frames(resMat(:,1)==cam) = frames(resMat(:,1)==cam) + startTimes(cam) - 1;
+end
+
+
+% Filter rows by frame interval
+resMat = resMat(ismember(frames, testInterval),:);
+
+for cam = 1:8
+    gtMat(gtMat(:,1) == cam & ~ismember(gtMat(:,3) + startTimes(cam) - 1, testInterval),:) = [];
+end
+
+% Filter rows by feet position within ROI
+feetpos = [ resMat(:,4) + 0.5*resMat(:,6), resMat(:,5) + resMat(:,7)];
+keep = false(size(resMat,1),1);
+for cam = 1:8
+    camFilter = resMat(:,1) == cam;
+    keep(camFilter & inpolygon(feetpos(:,1),feetpos(:,2), ROI{cam}(:,1),ROI{cam}(:,2))) = true;
+end
+
+resMat = resMat(keep,:);
+    
+
+% Single-Cam
+for camera = 1:8
+    
+    resMatSingle = resMat(resMat(:,1)==camera, 2:9);
+    gtMatSingle = gtMat(gtMat(:,1)==camera, 2:9);
+    [IDP, IDR, IDF1] = IDmeasures(resMatSingle, gtMatSingle, iou_threshold, world);
+    
+    result{camera}.IDP= IDP;
+    result{camera}.IDR = IDR;
+    result{camera}.IDF1= IDF1;
+    result{camera}.description = sprintf('Cam_%d',camera);
+    result{camera}.allMets = evaluateTracking(result{camera}.description, gtMatSingle, resMatSingle);
+    result{camera}.allMets.mets2d.m = [IDF1, IDP, IDR, result{camera}.allMets.mets2d.m];
+
+    
+end
+
+
+
+% Multi-Cam
+
+% Convert data format to:
+% ID, frame, left, top, width, height, worldX, worldY
+SHIFT_CONSTANT = 100000000;
+
+gtMatMulti  = gtMat(:,2:9);
+resMatMulti = resMat(:,2:9);
+gtMatMulti(:,2) = gtMat(:,3) + gtMat(:,1)*SHIFT_CONSTANT; % frame + cam*1000000 for frame uniqueness
+resMatMulti(:,2) = resMat(:,3) + resMat(:,1)*SHIFT_CONSTANT; 
+[IDP, IDR, IDF1] = IDmeasures(resMatMulti, gtMatMulti, iou_threshold, world);
+result{10}.IDP= IDP;
+result{10}.IDR = IDR;
+result{10}.IDF1= IDF1;
+result{10}.description = 'Multi-cam';
+
+% AllCameraSingle (MC Upper bound) 
+gtMatSingleAll = gtMat(:,2:9);
+resMatSingleAll = resMat(:,2:9);
+gtMatSingleAll(:,1) = gtMatSingleAll(:,1) + gtMat(:,1)*SHIFT_CONSTANT; % ID + cam*1000000 for ID uniqueness
+resMatSingleAll(:,1) = resMatSingleAll(:,1) + resMat(:,1)*SHIFT_CONSTANT;
+
+[IDP, IDR, IDF1] = IDmeasures(resMatSingleAll, gtMatSingleAll, iou_threshold, world);
+result{9}.IDP= IDP;
+result{9}.IDR = IDR;
+result{9}.IDF1= IDF1;
+result{9}.description = 'Single-all';
+result{9}.allMets = evaluateTracking(result{9}.description, gtMatSingleAll, resMatSingleAll);
+result{9}.allMets.mets2d.m = [IDF1, IDP, IDR, result{9}.allMets.mets2d.m];
+
